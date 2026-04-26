6efdfef9e2302e4d3e04a7b4704af273d061260a
Ichrafsassi <ichraf.sassi.456@gmail.com>
2026-04-26 19:45:40 +0100

    New version

--- js/ride.js
+++ js/ride.js
@@ -4,104 +4,122 @@ var RideFlow = window.RideFlow || {};
 RideFlow.map = RideFlow.map || {};
 
 (function rideScopeWrapper($) {
-    var authToken;
-    RideFlow.authToken.then(function setAuthToken(token) {
-        if (token) {
-            authToken = token;
-        } else {
-            window.location.href = '/signin.html';
-        }
-    }).catch(function handleTokenError(error) {
-        alert(error);
-        window.location.href = '/signin.html';
+  var authToken;
+  RideFlow.authToken
+    .then(function setAuthToken(token) {
+      if (token) {
+        authToken = token;
+      } else {
+        window.location.href = "/signin.html";
+      }
+    })
+    .catch(function handleTokenError(error) {
+      alert(error);
+      window.location.href = "/signin.html";
     });
-    function requestUnicorn(pickupLocation) {
-        $.ajax({
-            method: 'POST',
-            url: _config.api.invokeUrl + '/ride',
-            headers: {
-                Authorization: authToken
-            },
-            data: JSON.stringify({
-                PickupLocation: {
-                    Latitude: pickupLocation.latitude,
-                    Longitude: pickupLocation.longitude
-                }
-            }),
-            contentType: 'application/json',
-            success: completeRequest,
-            error: function ajaxError(jqXHR, textStatus, errorThrown) {
-                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
-                console.error('Response: ', jqXHR.responseText);
-                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
-            }
-        });
-    }
+  function requestRide(pickupLocation) {
+    $.ajax({
+      method: "POST",
+      url: _config.api.invokeUrl + "/ride",
+      headers: {
+        Authorization: authToken,
+      },
+      data: JSON.stringify({
+        PickupLocation: {
+          Latitude: pickupLocation.latitude,
+          Longitude: pickupLocation.longitude,
+        },
+      }),
+      contentType: "application/json",
+      success: completeRequest,
+      error: function ajaxError(jqXHR, textStatus, errorThrown) {
+        console.error(
+          "Error requesting ride: ",
+          textStatus,
+          ", Details: ",
+          errorThrown,
+        );
+        console.error("Response: ", jqXHR.responseText);
+        alert(
+          "An error occured when requesting your ride:\n" + jqXHR.responseText,
+        );
+      },
+    });
+  }
 
-    function completeRequest(result) {
-        var unicorn;
-        var pronoun;
-        console.log('Response received from API: ', result);
-        unicorn = result.Unicorn;
-        pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
-        displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.');
-        animateArrival(function animateCallback() {
-            displayUpdate(unicorn.Name + ' has arrived. Giddy up!');
-            RideFlow.map.unsetLocation();
-            $('#request').prop('disabled', 'disabled');
-            $('#request').text('Set Pickup');
-        });
-    }
+  function completeRequest(result) {
+    var driver;
+    var pronoun;
+    console.log("Response received from API: ", result);
+    driver = result.Unicorn;
+    pronoun = driver.Gender === "Male" ? "his" : "her";
+    displayUpdate(
+      driver.Name +
+        ", your " +
+        driver.Color +
+        " RideFlow driver, is on " +
+        pronoun +
+        " way.",
+    );
+    animateArrival(function animateCallback() {
+      displayUpdate(driver.Name + " has arrived. Your ride is ready.");
+      RideFlow.map.unsetLocation();
+      $("#request").prop("disabled", "disabled");
+      $("#request").text("Set Pickup");
+    });
+  }
 
-    // Register click handler for #request button
-    $(function onDocReady() {
-        $('#request').click(handleRequestClick);
-        $(RideFlow.map).on('pickupChange', handlePickupChanged);
+  // Register click handler for #request button
+  $(function onDocReady() {
+    $("#request").click(handleRequestClick);
+    $(RideFlow.map).on("pickupChange", handlePickupChanged);
 
-        RideFlow.authToken.then(function updateAuthMessage(token) {
-            if (token) {
-                displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
-                $('.authToken').text(token);
-            }
-        });
-
-        if (!_config.api.invokeUrl) {
-            $('#noApiMessage').show();
-        }
+    RideFlow.authToken.then(function updateAuthMessage(token) {
+      if (token) {
+        displayUpdate(
+          'You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.',
+        );
+        $(".authToken").text(token);
+      }
     });
 
-    function handlePickupChanged() {
-        var requestButton = $('#request');
-        requestButton.text('Request Unicorn');
-        requestButton.prop('disabled', false);
+    if (!_config.api.invokeUrl) {
+      $("#noApiMessage").show();
+    }
+  });
+
+  function handlePickupChanged() {
+    var requestButton = $("#request");
+    requestButton.text("Request Ride");
+    requestButton.prop("disabled", false);
+  }
+
+  function handleRequestClick(event) {
+    var pickupLocation = RideFlow.map.selectedPoint;
+    event.preventDefault();
+    requestRide(pickupLocation);
+  }
+
+  function animateArrival(callback) {
+    var dest = RideFlow.map.selectedPoint;
+    var origin = {};
+
+    if (dest.latitude > RideFlow.map.center.latitude) {
+      origin.latitude = RideFlow.map.extent.minLat;
+    } else {
+      origin.latitude = RideFlow.map.extent.maxLat;
     }
 
-    function handleRequestClick(event) {
-        var pickupLocation = RideFlow.map.selectedPoint;
-        event.preventDefault();
-        requestUnicorn(pickupLocation);
+    if (dest.longitude > RideFlow.map.center.longitude) {
+      origin.longitude = RideFlow.map.extent.minLng;
+    } else {
+      origin.longitude = RideFlow.map.extent.maxLng;
     }
 
-    function animateArrival(callback) {
-        var dest = RideFlow.map.selectedPoint;
-        var origin = {};
+    RideFlow.map.animate(origin, dest, callback);
+  }
 
-        if (dest.latitude > RideFlow.map.center.latitude) {
-            origin.latitude = RideFlow.map.extent.minLat;
-        } else {
-            origin.latitude = RideFlow.map.extent.maxLat;
-        }
-
-        if (dest.longitude > RideFlow.map.center.longitude) {
-            origin.longitude = RideFlow.map.extent.minLng;
-        } else {
-            origin.longitude = RideFlow.map.extent.maxLng;
-        }
-
-        RideFlow.map.animate(origin, dest, callback);
-    }
-
-    function displayUpdate(text) {
-        $('#updates').append($('<li>' + text + '</li>'));
-    }
-}(jQuery));
+  function displayUpdate(text) {
+    $("#updates").append($("<li>" + text + "</li>"));
+  }
+})(jQuery);
